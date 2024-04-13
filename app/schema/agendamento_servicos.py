5a5b2163fcc5e52b47af8ecc7afa9260fe8297b9
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2024-04-13 18:03:00 -0300

    Validate schema before process request

--- app/schema/agendamento_servicos.py
+++ app/schema/agendamento_servicos.py
@@ -1,16 +1,49 @@
-from pydantic import BaseModel, RootModel
+from pydantic import BaseModel, RootModel, validator
 from typing import List
+from datetime import datetime
 
 class NovoAgendamentoServicoSchema(BaseModel):
     """
     Define o contrato de inserção de um novo agendamento do serviço.
     """
-    data_agendamento: str = '2024-04-08 20:34:00'
+    data_agendamento: str = '2024-04-08 20:00:00'
     nome_cliente: str = 'Vitória Tenorio'
     nome_pet: str = 'Cacau'
     valor_servico: float = 50.0
     servico_id: int = 1
     cancelado: bool = False
+
+    @validator('data_agendamento')
+    def data_agendamento_deve_ser_valido(cls, value):
+        # Valida se a data_agendamento está em um formato válido.
+        try:
+            _ = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+            return value
+        except Exception as e:
+            # se deu erro a tentativa de conversão
+            raise ValueError('Formato de data e hora inválido')
+        
+    @validator('nome_cliente')
+    def nome_cliente_deve_ser_informado(cls, value):
+        # Valida se a data_agendamento está em um formato válido.
+        if len(value) < 1:
+            raise ValueError('Nome do cliente deve ser informado!')
+        return value
+        
+    @validator('nome_pet')
+    def nome_pet_deve_ser_informado(cls, value):
+        # Valida se o nome_pet foi informado.
+        if len(value) < 1:
+            raise ValueError('Nome do pet deve ser informado!')
+        return value
+        
+    @validator('valor_servico')
+    def valor_servico_deve_ser_maior_zero(cls, value: float):
+        # Valida se valor do serviço é válido.
+        if value < 0.0:
+            raise ValueError('Valor do serviço não pode ser zero!')
+        return value
+            
 
 class AlterarAgendamentoServicoSchema(BaseModel):
     """
